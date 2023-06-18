a4f0cff13f12b9bf303a1dcd691eaa2e27859d16
atanda0x <atandanafiu21@gmail.com>
2023-06-18 21:28:56 +0100

    added bot token

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	err := config.ReadConfig()
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	bot.Start()
+
+	<-make(chan struct{})
+	return
+}
